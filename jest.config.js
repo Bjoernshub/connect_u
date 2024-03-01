a5ea41d5e5b9578b6f31dd418fa55f2d8ae55117
Bjoern Zorkin <bjoern.zorkin@gmail.com>
2024-03-01 16:09:36 +0100

    jest config changes

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
-module.exports = {
-    preset: 'react-native',
-    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-    transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-      },      
-    moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-      },
-    transformIgnorePatterns: [
+const jestConfig = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transformIgnorePatterns: [
       'node_modules/(?!(react-native'
       + '|@react-native'
       + '|react-navigation-tabs'
@@ -16,6 +16,7 @@ module.exports = {
       + '|react-native-reanimated'
       + ')/)',
       'node_modules/(?!(react-native|expo-constants|@react-navigation|@expo/vector-icons)/)',
-    ],
-  };
-  
+  ],
+};
+
+export default jestConfig;
