f9da8fb0c73453baaf9eb3c88a12884e4350a463
Bjoern Zorkin <bjoern.zorkin@gmail.com>
2024-03-01 12:23:51 +0100

    changes on vitest an vite config files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
-    // Vitest test configuration options go here
     globals: true,
     environment: 'jsdom',
   },
